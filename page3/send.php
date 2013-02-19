ae04952c856407dc096838d45bb2c66231b87d15
copyhold <novojilov.ilya@gmail.com>
2013-02-19 15:18:43 +0200

    a

--- page3/send.php
+++ page3/send.php
@@ -9,6 +9,6 @@ Email: {$_GET['liame']}
 Subject: {$_GET['subj']}
 Message: {$_GET['text']}
 EOM;
-$headers = "Cc:optiwisemcc@gmail.com,boris.volfman@leshem-shamaim.com\r\nFrom: {$_GET['liame'] }\r\n";
+$headers = "Cc: optiwisemcc@gmail.com, boris.volfman@leshem-shamaim.com\r\nFrom: {$_GET['liame'] }\r\n";
 if (!mail('boris.volfman@leshem-shamaim.com', 'Guidance Request from ' . $region, $message, $headers)) die('mail not sent');
 echo json_encode(array("reply"=>"Your request is being processed."));
