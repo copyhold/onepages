48a5fbfeb7d0378de8f838f3b9d3dac786ce6834
copyhold <novojilov.ilya@gmail.com>
2013-05-21 23:19:57 +0300

    all+ie

--- page3/send.php
+++ page3/send.php
@@ -10,5 +10,7 @@ Subject: {$_GET['subj']}
 Message: {$_GET['text']}
 EOM;
 $headers = "Cc: khazanov29@gmail.com, omer@optiwise.co.il, yam@optiwise.co.il, boris.volfman@leshem-shamaim.com\r\nFrom: {$_GET['liame'] }\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
 if (!mail('boris.volfman@leshem-shamaim.com', 'Guidance Request from ' . $region, $message, $headers)) die('mail not sent');
 echo json_encode(array("reply"=>"Your request is being processed."));
